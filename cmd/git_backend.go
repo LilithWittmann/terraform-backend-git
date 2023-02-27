76f46522667c4583ef1d9fd0bea75843306dc68f
Lilith Wittmann <LilithWittmann@users.noreply.github.com>
2023-02-27 20:21:04 +0100

    Update git_backend.go

--- cmd/git_backend.go
+++ cmd/git_backend.go
@@ -9,10 +9,10 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"github.com/plumber-cd/terraform-backend-git/cmd/discovery"
-	"github.com/plumber-cd/terraform-backend-git/server"
+	"github.com/LilithWittmann/terraform-backend-git/cmd/discovery"
+	"github.com/LilithWittmann/terraform-backend-git/server"
 
-	_ "github.com/plumber-cd/terraform-backend-git/storages/git" // force it to init
+	_ "github.com/LilithWittmann/terraform-backend-git/storages/git" // force it to init
 )
 
 // gitHTTPBackendConfigPath is a path to the backend tf config to generate
